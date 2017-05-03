9c8735a604daf70eb93607a3082de42bd92e3912
niall coffey <niall300@yahoo.co.uk>
2017-05-03 22:17:29 +0100

    Add project files.

---
+++ HackerRankStaircase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankStaircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
